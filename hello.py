5eb10fa48c096b31865f0fe2277c87cea875eee4
xuev <xuewei918@gmail.com>
2018-04-12 12:35:05 +0800

    restart

--- hello.py
+++ hello.py
@@ -11,23 +11,38 @@
 @Description: 
 """
 
-from flask import Flask
-from flask_script import Manager
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+# @app.route('/browser')
+# def browser():
+#     user_agent = request.headers.get('User-Agent')
+#     return '<p>Your browser is %s</p>' % user_agent
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
