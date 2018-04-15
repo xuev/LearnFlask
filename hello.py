84fcdf07ee356818b45360cca7f7ecb77f7b87f2
xuev <xuewei918@gmail.com>
2018-04-15 17:56:41 +0800

    End Chapter.

--- hello.py
+++ hello.py
@@ -11,11 +11,10 @@
 @Description: 
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
-from flask_wtf import Form
+from flask_wtf.form import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -30,9 +29,16 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
